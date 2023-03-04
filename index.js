318a3c778ec0ccf13dfb186edf252990ad896cd6
Didi <didi@pop-os.localdomain>
2023-03-04 01:53:27 +0000

    fixed issue with adding SSL key and now list connected devices

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as JoyCon from './node_modules/joy-con-webhid/dist/index.js';
+import * as JoyCon from './node_modules/joy-con-webhid/src/index.js';
 
-let device_list = []
+let device_list = ""
 // For the initial pairing of the Joy-Cons. They need to be paired one by one.
 // Once paired, Joy-Cons will be reconnected to on future page loads.
 window.onload=function(){
@@ -8,11 +8,17 @@ window.onload=function(){
     // `JoyCon.connectJoyCon()` handles the initial HID pairing.
     // It keeps track of connected Joy-Cons in the `JoyCon.connectedJoyCons` Map.
     await JoyCon.connectJoyCon();
-    devices = JoyCon.connectedJoyCons.values();
-    updateDeviceList()
+    for (const joyCon of JoyCon.connectedJoyCons.values()) {
+      console.log("Joycon connect: " + joyCon.device.productName)
+      if ((joyCon.device.productName != undefined))
+        if (device_list.search(joyCon.device.productName) == -1) {
+          updateDeviceList(joyCon.device.productName)
+        }
+    }
+
   });
 };
-// Joy-Cons may sleep until touched and fall asleep again if idle, so attach
+// Joy-Cons may sleep until touched a2nd fall asleep again if idle, so attach
 // the listener dynamically, but only once.
 setInterval(async () => {
   for (const joyCon of JoyCon.connectedJoyCons.values()) {
@@ -26,21 +32,22 @@ setInterval(async () => {
     await joyCon.enableIMUMode();
     await joyCon.enableVibration();
     // Get information about the connected Joy-Con.
-    console.log(await joyCon.getRequestDeviceInfo());
+   // console.log(await joyCon.getRequestDeviceInfo());
     // Rumble.
     await joyCon.rumble(600, 600, 0.5);
     // Listen for HID input reports.
     joyCon.addEventListener('hidinput', ({ detail }) => {
       // Careful, this fires at ~60fps.
-      console.log(`Input report from ${joyCon.device.productName}:`, detail);
+     // console.log(`Input report from ${joyCon.device.productName}:`, detail);
     });
     joyCon.eventListenerAttached = true;
+    console.log("Joycon open: " + joyCon.device.productName)
   }
 }, 2000);
 
-const updateDeviceList = () => {
-  document.querySelector('#connected-devices').innerHTML = device_list.map(device=> {
-    return `<li>${device.productName}${device.opened ? " (ready)" : ""}</li>`;
-  })
-  .join("");
+const updateDeviceList = (name) => {
+  let s = "";
+  s = '<li>' + name + '(ready)  </li>';
+  device_list += s
+  document.querySelector('#connected-devices').innerHTML = device_list;
 };
