19c57775df73506947aa5cf11293ffc6e733fe36
Daniel Brown <2469283b@student.gla.ac.uk>
2023-03-28 22:14:37 +0100

    Replicated the demo and removed some annoying stuff with the visuals. Need to figure out direction and implement a 'jump' counter

--- index.js
+++ index.js
@@ -1,46 +1,100 @@
-import * as JoyCon from './node_modules/joy-con-webhid/dist/index.js';
+import {
+  connectJoyCon,
+  connectedJoyCons,
+  JoyConLeft,
+  JoyConRight,
+  GeneralController,
+} from './node_modules/joy-con-webhid/src/index.js';
 
-let device_list = []
-// For the initial pairing of the Joy-Cons. They need to be paired one by one.
-// Once paired, Joy-Cons will be reconnected to on future page loads.
-window.onload=function(){
-  document.querySelector('#connect-button').addEventListener('click', async () => {
-    // `JoyCon.connectJoyCon()` handles the initial HID pairing.
-    // It keeps track of connected Joy-Cons in the `JoyCon.connectedJoyCons` Map.
-    await JoyCon.connectJoyCon();
-    devices = JoyCon.connectedJoyCons.values();
-    updateDeviceList()
-  });
+const connectButton = document.querySelector('#connect-joy-cons');
+const debugLeft = document.querySelector('#debug-left');
+const debugRight = document.querySelector('#debug-right');
+const showDebug = document.querySelector('#show-debug');
+const rootStyle = document.documentElement.style;
+
+document.querySelector('#joycon-l').style.visibility = 'hidden';
+document.querySelector('#joycon-r').style.visibility = 'hidden';
+connectButton.addEventListener('click', connectJoyCon);
+
+const visualize = (joyCon, packet) => {
+  if (!packet || !packet.actualOrientation) {
+    return;
+  }
+  const {
+    actualAccelerometer: accelerometer,
+    buttonStatus: buttons,
+    actualGyroscope: gyroscope,
+    actualOrientation: orientation,
+    actualOrientationQuaternion: orientationQuaternion,
+  } = packet;
+
+  if (joyCon instanceof JoyConLeft || joyCon instanceof GeneralController) {
+    document.querySelector('#joycon-l').style.visibility = 'visible';
+  }
+  if (joyCon instanceof JoyConRight || joyCon instanceof GeneralController) {
+    document.querySelector('#joycon-r').style.visibility = 'visible';
+  }
+
+  // test led and rumble
+  if (buttons.a || buttons.up) {
+    joyCon.blinkLED(0);
+  }
+  if (buttons.b || buttons.down) {
+    joyCon.setLED(0);
+  }
+  if (buttons.x || buttons.right) {
+    joyCon.resetLED(0);
+    joyCon.rumble(600, 600, 0);
+  }
+  if (buttons.y || buttons.left) {
+    joyCon.rumble(600, 600, 0.5);
+  }
+
+  if (showDebug.checked) {
+    const controller = joyCon instanceof JoyConLeft ? debugLeft : debugRight;
+    controller.querySelector('pre').textContent =
+      JSON.stringify(orientation, null, 2) +
+      '\n' +
+      JSON.stringify(orientationQuaternion, null, 2) +
+      '\n' +
+      JSON.stringify(gyroscope, null, 2) +
+      '\n' +
+      JSON.stringify(accelerometer, null, 2) +
+      '\n';
+    const meterMultiplier = 300;
+    controller.querySelector('#acc-x').value =
+      accelerometer.x * meterMultiplier;
+    controller.querySelector('#acc-y').value =
+      accelerometer.y * meterMultiplier;
+    controller.querySelector('#acc-z').value =
+      accelerometer.z * meterMultiplier;
+
+    const gyroscopeMultiplier = 300;
+    controller.querySelector('#gyr-x').value =
+      gyroscope.rps.x * gyroscopeMultiplier;
+    controller.querySelector('#gyr-y').value =
+      gyroscope.rps.y * gyroscopeMultiplier;
+    controller.querySelector('#gyr-z').value =
+      gyroscope.rps.z * gyroscopeMultiplier;
+  }
 };
-// Joy-Cons may sleep until touched and fall asleep again if idle, so attach
-// the listener dynamically, but only once.
+
+// Joy-Cons may sleep until touched, so attach the listener dynamically.
 setInterval(async () => {
-  for (const joyCon of JoyCon.connectedJoyCons.values()) {
+  for (const joyCon of connectedJoyCons.values()) {
     if (joyCon.eventListenerAttached) {
       continue;
     }
-    // Open the device and enable standard full mode and inertial measurement
-    // unit mode, so the Joy-Con activates the gyroscope and accelerometers.
-    await joyCon.open();
-    await joyCon.enableStandardFullMode();
-    await joyCon.enableIMUMode();
-    await joyCon.enableVibration();
-    // Get information about the connected Joy-Con.
-    console.log(await joyCon.getRequestDeviceInfo());
-    // Rumble.
-    await joyCon.rumble(600, 600, 0.5);
-    // Listen for HID input reports.
-    joyCon.addEventListener('hidinput', ({ detail }) => {
-      // Careful, this fires at ~60fps.
-      console.log(`Input report from ${joyCon.device.productName}:`, detail);
-    });
     joyCon.eventListenerAttached = true;
+    await joyCon.enableVibration();
+    joyCon.addEventListener('hidinput', (event) => {
+      visualize(joyCon, event.detail);
+    });
   }
 }, 2000);
 
-const updateDeviceList = () => {
-  document.querySelector('#connected-devices').innerHTML = device_list.map(device=> {
-    return `<li>${device.productName}${device.opened ? " (ready)" : ""}</li>`;
-  })
-  .join("");
-};
+showDebug.addEventListener('input', (e) => {
+  document.querySelector('#debug').style.display = e.target.checked
+    ? 'flex'
+    : 'none';
+});
